d73fc3f969ee37e37475bf8adb565d45643de86d
BenHampton <BenHampton11@yahoo.com>
2019-02-22 20:29:55 -0600

    initial push

---
+++ Spring-Security-Oauth2/src/main/java/com/springsecurityoauth2/springsecurityoauth2/config/SecurityConfig.java
@@ -0,0 +1,12 @@
+package com.springsecurityoauth2.springsecurityoauth2.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@EnableResourceServer
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
